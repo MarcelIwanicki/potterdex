987656a71a05c93487acc6aa4a0896ca8aeccd76
CzarnyAlibaba <iwanickimarcel@onet.eu>
2021-09-06 21:33:18 +0200

    PD-8: Filtering by categories implementation

--- lib/feature/dashboard/business_logic/bloc/dashboard_bloc.dart
+++ lib/feature/dashboard/business_logic/bloc/dashboard_bloc.dart
@@ -18,19 +18,84 @@ class DashboardBloc extends Bloc<DashboardEvent, DashboardState> {
   Stream<DashboardState> mapEventToState(
     DashboardEvent event,
   ) async* {
-    if (event is GetHarryPotterCharactersDashboardEvent) {
-      yield HarryPotterCharactersAreLoadingState();
+    yield HarryPotterCharactersAreLoadingState();
 
+    Function(List<HarryPotterCharacter>) filterFunction =
+        _getFilterFunctionBasedOnEvent(event);
+
+    if (event is ResetHarryPotterCharactersDashboardEvent) {
+      yield HarryPotterCharactersAreNotLoadedState();
+    } else {
       try {
         await service.loadHarryPotterCharactersFromRemoteToLocalDatabase();
         List<HarryPotterCharacter> harryPotterCharacters =
             await service.getHarryPotterCharactersFromLocalDatabase();
-        yield HarryPotterCharactersAreLoadedState(harryPotterCharacters);
+        List<HarryPotterCharacter> filteredHarryPotterCharacters =
+            filterFunction(harryPotterCharacters);
+        yield HarryPotterCharactersAreLoadedState(
+            filteredHarryPotterCharacters);
       } catch (e) {
         yield HarryPotterCharactersAreNotLoadedState();
       }
-    } else if (event is ResetHarryPotterCharactersDashboardEvent) {
-      yield HarryPotterCharactersAreNotLoadedState();
     }
   }
+
+  Function(List<HarryPotterCharacter>) _getFilterFunctionBasedOnEvent(
+      DashboardEvent event) {
+    if (event is FilterHarryPotterCharactersDashboardByFavoriteEvent) {
+      return _filterCharactersByFavorite;
+    } else if (event is FilterHarryPotterCharactersDashboardByHumanEvent) {
+      return _filterCharactersByHuman;
+    } else if (event is FilterHarryPotterCharactersDashboardByFemaleEvent) {
+      return _filterCharactersByFemale;
+    } else if (event is FilterHarryPotterCharactersDashboardByMaleEvent) {
+      return _filterCharactersByMale;
+    } else if (event is FilterHarryPotterCharactersDashboardByGryffindorEvent) {
+      return _filterCharactersByGryffindor;
+    } else if (event is FilterHarryPotterCharactersDashboardByHufflepuffEvent) {
+      return _filterCharactersByHufflepuff;
+    } else if (event is FilterHarryPotterCharactersDashboardBySlytherinEvent) {
+      return _filterCharactersBySlytherin;
+    } else if (event is FilterHarryPotterCharactersDashboardByRavenclawEvent) {
+      return _filterCharactersByRavenclaw;
+    }
+
+    return _dontFilter;
+  }
+
+  List<HarryPotterCharacter> _dontFilter(
+          List<HarryPotterCharacter> characters) =>
+      characters;
+
+  List<HarryPotterCharacter> _filterCharactersByFavorite(
+          List<HarryPotterCharacter> characters) =>
+      characters.where((element) => element.favorite).toList();
+
+  List<HarryPotterCharacter> _filterCharactersByHuman(
+          List<HarryPotterCharacter> characters) =>
+      characters.where((element) => element.species == "human").toList();
+
+  List<HarryPotterCharacter> _filterCharactersByFemale(
+          List<HarryPotterCharacter> characters) =>
+      characters.where((element) => element.gender == "female").toList();
+
+  List<HarryPotterCharacter> _filterCharactersByMale(
+          List<HarryPotterCharacter> characters) =>
+      characters.where((element) => element.gender == "male").toList();
+
+  List<HarryPotterCharacter> _filterCharactersByGryffindor(
+          List<HarryPotterCharacter> characters) =>
+      characters.where((element) => element.house == "Gryffindor").toList();
+
+  List<HarryPotterCharacter> _filterCharactersByHufflepuff(
+          List<HarryPotterCharacter> characters) =>
+      characters.where((element) => element.house == "Hufflepuff").toList();
+
+  List<HarryPotterCharacter> _filterCharactersBySlytherin(
+          List<HarryPotterCharacter> characters) =>
+      characters.where((element) => element.house == "Slytherin").toList();
+
+  List<HarryPotterCharacter> _filterCharactersByRavenclaw(
+          List<HarryPotterCharacter> characters) =>
+      characters.where((element) => element.house == "Ravenclaw").toList();
 }
